c4a7c7b3955e3b8aff45b102e3da9fb0d194d1eb
Uwe Plonus <u.plonus@gmail.com>
2018-11-09 22:31:48 +0100

    Added Locale to test because it was language dependent

--- jdip_168r1/src/test/java/info/jdip/datc/BordersTest.java
+++ jdip_168r1/src/test/java/info/jdip/datc/BordersTest.java
@@ -4,11 +4,13 @@ import dip.misc.TestSuite;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.File;
+import java.util.Locale;
 
 public class BordersTest extends DATCTest {
 
     @BeforeAll
     public static void setTestCaseLocation(){
+        Locale.setDefault(Locale.US);
         testSuite = new TestSuite(new File("build/distributions"));
         testCaseLocation = "etc/test_data/borders.txt";
         testSuite.parseCases(new File(testCaseLocation));
